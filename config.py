32b66b2ed38a4ba9d8201f9c858d0cbb0d71844e
kwteh <khaiwenteh@gmail.com>
2023-04-13 15:10:54 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.criyg2muhvph.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tehkhaiwen-employee"
+customregion = "us-east-1"
